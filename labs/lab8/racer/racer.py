6d9a94790f3866d342601b426edf90c2583d2342
Dana <kajratdana352@gmail.com>
2025-03-21 22:46:42 +0500

    lab 8

--- labs/lab8/racer/racer.py
+++ labs/lab8/racer/racer.py
@@ -2,13 +2,16 @@ import pygame
 import random
 import time
 
+# Initialize pygame
 pygame.init()
 
+# Set screen dimensions
 WIDTH = 400
 HEIGHT = 600
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+#load images
 image_player = pygame.image.load("resources/Player.png")
 image_enemy = pygame.image.load("resources/Enemy.png")
 image_road = pygame.image.load("resources/AnimatedStreet.png")
@@ -16,6 +19,8 @@ image_road = pygame.image.load("resources/AnimatedStreet.png")
 image_coin = pygame.image.load("resources/coin.png")
 image_coin = pygame.transform.scale(image_coin, (50, 50))
 
+
+#load music and sounds
 pygame.mixer.music.load("resources/background.wav")
 sound_crach = pygame.mixer.Sound("resources/crash.wav")
 sound_coin = pygame.mixer.Sound("resources/coin_sound.wav")
@@ -23,64 +28,71 @@ sound_game_over = pygame.mixer.Sound("resources/game_over.mp3")
 
 pygame.mixer.music.play(-1)
 
-
+# Define fonts and text
 font = pygame.font.SysFont("Verdana", 60)
 image_game_over = font.render("Game Over!", True, "black")
 image_game_over_rect = image_game_over.get_rect(center = (WIDTH // 2, HEIGHT // 2))
 
 
-
+# Set up game clock
 clock = pygame.time.Clock()
 FPS = 60
 
 running = True
 
+
+# Define game parameters
 PLAYER_SPEED = 5
 ENEMY_SPEED = 10
 COIN_SPEED = random.randint(5, 10)
 
+
+# Coin counter
 earned_coins = 0
 font2 = pygame.font.SysFont("Verdana", 20)
 coin_counter = font2.render(f"Coins: {earned_coins}", True, "red")
 coin_counter_rect = coin_counter.get_rect(center = (WIDTH - 100, 32))
 
+
+#Player class
 class Player(pygame.sprite.Sprite):
         def __init__(self):
               super().__init__()
               self.image = image_player
               self.rect = self.image.get_rect()
-              self.rect.centerx = WIDTH // 2
+              self.rect.centerx = WIDTH // 2  # position of the car
               self.rect.bottom = HEIGHT
         
         def move(self):
                 keys = pygame.key.get_pressed()
-                if keys[pygame.K_LEFT]:
+                if keys[pygame.K_LEFT]: # move left
                      self.rect.move_ip(-PLAYER_SPEED, 0)
-                if keys[pygame.K_RIGHT]:
+                if keys[pygame.K_RIGHT]: # move right
                      self.rect.move_ip(PLAYER_SPEED, 0)
                 if self.rect.left < 0:
-                       self.rect.left = 0   
+                       self.rect.left = 0  
                 if self.rect.right > WIDTH:
                        self.rect.right = WIDTH    
 
-
+# Enemy class
 class Enemy(pygame.sprite.Sprite):
         def __init__(self):
               super().__init__()
               self.image = image_enemy
-              self.rect = self.image.get_rect()
+              self.rect = self.image.get_rect() 
               self.generate_rand_rect()
 
         def generate_rand_rect(self):
-               self.rect.left = random.randint(0, WIDTH - self.rect.w)
+               self.rect.left = random.randint(0, WIDTH - self.rect.w) #generate an enemy car in random position on the top
                self.rect.bottom = 0
 
         def move(self):
-               self.rect.move_ip(0, ENEMY_SPEED)
-               if self.rect.top > HEIGHT:
+               self.rect.move_ip(0, ENEMY_SPEED) # moves downwards
+               if self.rect.top > HEIGHT:  # Reset enemy position when it leaves the screen
                       self.generate_rand_rect()
               
 
+# Coin class
 class Coin(pygame.sprite.Sprite):
         def __init__(self):
               super().__init__()
@@ -88,32 +100,33 @@ class Coin(pygame.sprite.Sprite):
               self.rect = self.image.get_rect()
               self.generate_rand_rect()
               self.respawn_time = 0
-              self.next_spawn_delay = random.randint(4000, 7000)
+              self.next_spawn_delay = random.randint(4000, 7000) #set random spawnin delay
         
         def generate_rand_rect(self):
-              self.rect.left = random.randint(0, WIDTH - self.rect.w)
+              self.rect.left = random.randint(0, WIDTH - self.rect.w) #generate coin on random position on the top
               self.rect.bottom = 0
         
         def move(self):
                if pygame.time.get_ticks()  < self.respawn_time:
                       return
 
-               self.rect.move_ip(0, COIN_SPEED)
-               if self. rect.top > HEIGHT:
+               self.rect.move_ip(0, COIN_SPEED) #moves downwards
+               if self. rect.top > HEIGHT: # Respawn coin after delay
                       self.respawn_time = pygame.time.get_ticks() + self.next_spawn_delay
                       self.generate_rand_rect()
 
 
-
+# Game objects
 enemy = Enemy()
 player = Player()
 coin = Coin()
 
-
+# Create sprite groups
 all_sprites = pygame.sprite.Group()
 enemy_sprites = pygame.sprite.Group()
 coin_sprites = pygame.sprite.Group()
 
+# Add objects to sprite groups
 all_sprites.add(player, enemy, coin)
 enemy_sprites.add(enemy)
 coin_sprites.add(coin)
@@ -123,43 +136,51 @@ while running:
                 if event.type == pygame.QUIT:
                         running = False
 
+        #move player
         player.move()
 
+        #draw background
         screen.blit(image_road, (0, 0))
 
+        #update and display coin counter
         coin_counter = font2.render(f"Coins: {earned_coins}", True, "red")
         screen.blit(coin_counter, coin_counter_rect)
         
         
+        # Move and draw all game objects
         for entity in all_sprites:
                entity.move()
                screen.blit(entity.image, entity.rect)
 
         
-
+        # Check collision between player and enemy
         if pygame.sprite.spritecollideany(player, enemy_sprites):
                sound_crach.play()
-               time.sleep(1)
+               time.sleep(1) # wait 1 sec
 
+               #stop the game loop
                running = False
 
+               # Stop background music and play game over sound
                pygame.mixer.music.stop()
                sound_game_over.play()
 
+               # Display game over screen
                screen.fill("red")
                screen.blit(image_game_over, image_game_over_rect)
                pygame.display.flip()
 
                time.sleep(3)
 
+        # Check collision between player and coin
         if pygame.sprite.spritecollideany(player, coin_sprites):
-               sound_coin.play()
-               earned_coins += 100
-               coin.respawn_time = pygame.time.get_ticks() + random.randint(2000, 5000)
+               sound_coin.play() #play coin earning sound
+               earned_coins += 100 #increase score
+               coin.respawn_time = pygame.time.get_ticks() + random.randint(2000, 5000) #set random spawn delay
                coin.generate_rand_rect()
 
         
         
-         
+        # Update display and set FPS 
         pygame.display.flip()
         clock.tick(FPS)
--- labs/lab8/snake.py
+++ labs/lab8/snake.py
@@ -3,9 +3,10 @@ import random
 from pygame.math import Vector2
 import time
 
+# Initialize pygame
 pygame.init()
 
-
+# Set screen dimensions
 WIDTH = 600
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,23 +15,24 @@ clock = pygame.time.Clock()
 FPS = 60
 running = True
 
+# cell size for the grid
 CELL = 30
 
-colorWHITE = (255, 255, 255)
-colorGRAY = (200, 200, 200)
-colorBLACK = (0, 0, 0)
+# Colors
 colorRED = (255, 0, 0)
-colorBLUE = (0, 0, 255)
 colorYELLOW = (255, 255, 0)
 colorORANGE = (255, 165, 0)
-colorGREEN1 =  (220, 220, 200)
-colorGREEN2 = (140, 140, 125)
+colorGREED1 =  (220, 220, 200)
+colorGREED2 = (140, 140, 125)
 
+
+# Game over text
 font1 = pygame.font.SysFont("Verdana", 60)
 game_over_im = font1.render("GAME OVER", True, "black")
 game_over_rect = game_over_im.get_rect(center = (WIDTH // 2, HEIGHT // 2))
 
-#fruits
+
+# Score and Level counter
 score = 0
 font2 = pygame.font.SysFont("Verdana", 20)
 score_counter = font2.render(f"Score: {score}", True, "green")
@@ -42,21 +44,25 @@ level_counter = font3.render(f"Level: {level}", True, "green")
 level_counter_rect = level_counter.get_rect(center = (WIDTH - 100, 50))
 
 
+# Function to draw the chessboard grid
 def draw_grid_chess():
-    colors = [colorGREEN1, colorGREEN2]
+    colors = [colorGREED1, colorGREED2]
     for i in range(HEIGHT // 2):
         for j in range(WIDTH // 2):
             pygame.draw.rect(screen, colors[(i + j) % 2], (i * CELL, j * CELL, CELL, CELL))
 
 
+# Fruit class to represent food for the snake
 class Fruit:
     def __init__(self):
         self.pos = Vector2(-1, -1)
     def draw(self):
+        # drawing a fruit
         fruit_rect = pygame.Rect(int(self.pos.x * CELL,), int(self.pos.y * CELL), CELL, CELL)
         pygame.draw.rect(screen, colorRED, fruit_rect)
     
     def randomize(self, snake_body):
+         # Generate a random position for the fruit so it won't appear on the body of the snake
         while True:
             self.x = random.randint(0, WIDTH // CELL - 1)
             self.y = random.randint(0, HEIGHT // CELL - 1)
@@ -66,7 +72,7 @@ class Fruit:
                 break
 
 
-
+# Snake class to move and draw it
 class Snake:
     def __init__(self):
         self.body = [Vector2(10, 11), Vector2(10, 12), Vector2(10,13)]
@@ -74,30 +80,35 @@ class Snake:
         self.new_block = False
 
     def move(self):
-        if self.new_block:
+        if self.new_block: # Add a new block if the snake has eaten a fruit
             body_copy = self.body[:]
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
             self.new_block = False
 
+        # Move the snake by shifting positions
         body_copy = self.body[:-1]
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
 
     def draw(self):
+        # Draw the snake's head
         head = self.body[0]
         pygame.draw.rect(screen, colorORANGE, (int(head.x * CELL), int(head.y * CELL), CELL, CELL))
+        # Draw the snake's body
         for block in self.body[1:]:
             x_pos = int(block.x * CELL)
             y_pos = int(block.y * CELL)
             pygame.draw.rect(screen, colorYELLOW, (x_pos, y_pos, CELL, CELL))
 
     
-
+# Game speed to make it harder
 speed = 150
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, speed)
 
+
+# Main game logic
 class Main():
     def __init__(self):
         self.snake = Snake()
@@ -113,14 +124,17 @@ class Main():
         self.snake.draw()
         self.fruit.draw()
 
+    # Check if the snake eats the fruit
     def check_collision(self):
         global score, level, speed
         head = self.snake.body[0]
         if head == self.fruit.pos:
             self.fruit.randomize(self.snake.body)
             self.snake.new_block = True
+            # Increase score
             score += 100
             if score % 300 == 0:
+                # Increase level and adjust speed
                 level += 1
                 if speed >= 50:
                     speed -= 15
@@ -128,10 +142,10 @@ class Main():
             
     
     def check_fail(self):
-        #1
+        #1 Check if the snake collides with the wall
         if not 0 <= self.snake.body[0].x < WIDTH // CELL or not 0 <= self.snake.body[0].y < HEIGHT // CELL:
             self.game_over()
-        #2
+        #2 Check if the snake collides with itself
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
                 self.game_over()
@@ -157,7 +171,7 @@ while running:
             running = False
         if event.type == SCREEN_UPDATE:
             main_game.update()
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  # moving the snake
             if event.key == pygame.K_UP and main_game.snake.direction.y == 0:
                 main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN and main_game.snake.direction.y == 0:
@@ -167,12 +181,14 @@ while running:
             if event.key == pygame.K_RIGHT and main_game.snake.direction.x == 0:
                 main_game.snake.direction = Vector2(1, 0)
     
-    draw_grid_chess()
 
+    # Draw the game elements
+    draw_grid_chess()
     main_game.draw_elements()
 
+    # Display score and level
     score_counter = font2.render(f"Score: {score}", True, "green")
-    score_counter_rect = score_counter.get_rect(center = (WIDTH - 100, 30))
+    score_counter_rect = score_counter.get_rect(center = (WIDTH - 100, 30)) 
     screen.blit(score_counter, score_counter_rect)
 
     level_counter = font3.render(f"Level: {level}", True, "green")
